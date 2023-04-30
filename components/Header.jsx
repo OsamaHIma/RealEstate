da758f49fdff8855c84789a2e257f30beb940fc8
Osama Ibrahim <108794970+OsamaHIma@users.noreply.github.com>
2023-04-30 18:49:34 +0300

    Menus fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,19 +8,31 @@ import { useState } from "react";
 // h- stands for header-
 const Header = () => {
   const { setTheme } = useTheme();
-  const [showThemeMenu, setThemeMenu] = useState(false);
-  const [showMenu, setMenu] = useState(false);
+  const [showThemeMenu, setThemeMenu] = useState("-top-[400px]");
+  const [showMenu, setMenu] = useState("-right-[400px]");
+  // theme Menu toggler button
   const openThemeMenu = () => {
-    if (showMenu) {
-      setMenu(false);
+    if (showMenu === "right-0") {
+      setMenu("-right-[400px]");
     }
-    setThemeMenu(!showThemeMenu);
+
+    if (showThemeMenu === "top-[17%]") {
+      setThemeMenu("-top-[400px]");
+    } else {
+      setThemeMenu("top-[17%]");
+    }
   };
+  // Menu toggler button
   const openMenu = () => {
-    if (showThemeMenu) {
-      setThemeMenu(false);
+    if (showThemeMenu === "top-[17%]") {
+      setThemeMenu("-top-[400px]");
     }
-    setMenu(!showMenu);
+
+    if (showMenu === "right-0") {
+      setMenu("-right-[400px]");
+    } else {
+      setMenu("right-0");
+    }
   };
   return (
     <section className="h-wrapper transition-all ease-in text-slate-800 dark:text-white">
@@ -36,31 +48,32 @@ const Header = () => {
             <span className="sr-only">Toggle theme menu</span>
           </button>
 
-          {showThemeMenu && (
-            <ul className="flex w-[9rem] absolute right-[-50%] md:right-[60%] top-[27%] z-10 flex-col m-8 shadow-lg select-none">
-              <li
-                className="bg-blue-gradient pb-2 pt-4 rounded-t-md px-8 cursor-pointer hover:bg-slate-800 text-slate-100 hover:text-gray-300 active:!bg-gray-400"
-                onClick={() => setTheme("dark")}
-              >
-                <Moon className="mr-2 h-4 w-4" />
-                <span>Dark</span>
-              </li>
-              <li
-                className=" bg-blue-gradient py-2 pl-8 cursor-pointer hover:bg-slate-800 text-slate-100 hover:text-gray-300 active:!bg-gray-400"
-                onClick={() => setTheme("light")}
-              >
-                <Sun className="mr-2 h-4 w-4" />
-                <span>Light</span>
-              </li>
-              <li
-                className=" bg-blue-gradient pt-2 pb-4 rounded-b-md pl-8 cursor-pointer hover:bg-slate-800 text-slate-100 hover:text-gray-300 active:!bg-gray-400"
-                onClick={() => setTheme("system")}
-              >
-                <Laptop className="mr-2 h-4 w-4" />
-                <span>System</span>
-              </li>
-            </ul>
-          )}
+          <ul
+            className={`${showThemeMenu} menuTransition flex w-[9rem] absolute right-[-30%] md:right-[60%] z-10 flex-col m-8 shadow-lg select-none`}
+          >
+            <li
+              className="bg-blue-gradient pb-2 pt-4 rounded-t-md px-8 cursor-pointer hover:bg-slate-800 text-slate-100 hover:text-gray-300 active:!bg-gray-400"
+              onClick={() => setTheme("dark")}
+            >
+              <Moon className="mr-2 h-4 w-4" />
+              <span>Dark</span>
+            </li>
+            <li
+              className=" bg-blue-gradient py-2 pl-8 cursor-pointer hover:bg-slate-800 text-slate-100 hover:text-gray-300 active:!bg-gray-400"
+              onClick={() => setTheme("light")}
+            >
+              <Sun className="mr-2 h-4 w-4" />
+              <span>Light</span>
+            </li>
+            <li
+              className=" bg-blue-gradient pt-2 pb-4 rounded-b-md pl-8 cursor-pointer hover:bg-slate-800 text-slate-100 hover:text-gray-300 active:!bg-gray-400"
+              onClick={() => setTheme("system")}
+            >
+              <Laptop className="mr-2 h-4 w-4" />
+              <span>System</span>
+            </li>
+          </ul>
+
           {/* for large screens */}
           <div className="hidden lg:block">
             <ul className="h-menu flexCenter !gap-8">
@@ -79,18 +92,19 @@ const Header = () => {
             <MenuIcon className="hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100" />
             <span className="sr-only">Toggle menu</span>
           </button>
-          {showMenu && (
-            <ul className="flex absolute bg-slate-100 py-8 px-11 rounded-lg right-0 w-[15rem] top-[27%] z-10 flex-col m-8 shadow-lg">
-              {navLinks.map((link, index) => (
-                <li key={index} className="my-3">
-                  <Link href={`#${link.id}`}>{link.name}</Link>
-                </li>
-              ))}
-              <button type="button" className="btn bg-blue-gradient">
-                <Link href="#contact">Contact</Link>
-              </button>
-            </ul>
-          )}
+
+          <ul
+            className={`flex lg:hidden menuTransition ${showMenu} absolute bg-black/10 dark:bg-white/10 backdrop-blur-md py-8 px-11 rounded-lg w-[15rem] top-[27%] z-10 flex-col m-8 shadow-lg`}
+          >
+            {navLinks.map((link, index) => (
+              <li key={index} className="my-3">
+                <Link href={`#${link.id}`}>{link.name}</Link>
+              </li>
+            ))}
+            <button type="button" className="btn bg-blue-gradient">
+              <Link href="#contact">Contact</Link>
+            </button>
+          </ul>
         </nav>
       </div>
     </section>
